6221a28dd7a68b6c583cd8fddb7fbfbbb1dd09d8
ivan-paz <ivnpaz@gmail.com>
2018-01-10 05:10:06 +0100

    function

--- createConnectedComponents.py
+++ createConnectedComponents.py
@@ -3,28 +3,42 @@
 #    This function takes a rule Set and separates it into
 #    its connected Components, i.e. into sets with rules
 #    which intersects among them
+#
 #------------------------------------------------------------
 
 #Example1
-ruleSet = [
-        #Component 1
-	[ {1,3},   {3,5},  'A'],
-	[ {2,5},   {4},    'A'],
-	[ {2},     {2,6},  'A'],
-        #Component 2
-	[ {5.5,9}, {1},    'A'],
-        [ {7},  {0.5,2.5}, 'A'],
-        #Component 3
-        [ {8},     {4.5},  'A']
-]
+#ruleSet = [
+#        #Component 1
+#	[ {1,3},   {3,5},  'A'],
+#	[ {2,5},   {4},    'A'],
+#	[ {2},     {2,6},  'A'],
+#        #Component 2
+#	[ {5.5,9}, {1},    'A'],
+#        [ {7},  {0.5,2.5}, 'A'],
+#        #Component 3
+#        [ {8},     {4.5},  'A']
+#]
+
+#Example 1.1
+#ruleSet = [
+#        #Component 1
+#        [ {1,3},   {3,5},  'A'],
+#        [ {2,5},   {4},    'A'],
+#        [ {4},     {2,6},  'A'],#this has been moved
+#        #Component 2
+#        [ {5.5,9}, {1},    'A'],
+#        [ {7},  {0.5,2.5}, 'A'],
+#        #Component 3
+#        [ {8},     {4.5},  'A']
+#]
 
 #Example2
-#ruleSet = [
-#[ {1, 2, 3, 8, 11}, {4, 6},   'A'],
-#[ {9,12},            {5},     'A'],
-#[    {5},            {4},     'A'],
-#[ {2,5},             {7},     'A']
-#]
+ruleSet = [
+[ {1, 2, 3, 8, 11}, {4, 6},   'A'],
+[ {9,12},            {5},     'A'],
+[    {5},            {4},     'A'],
+[ {2,5},             {7},     'A']
+]
 
 # This function finds the minimum and maximum values
 # of a set containing numbers
@@ -54,11 +68,12 @@ def createConnectedComponents(ruleSet):
     while ruleSet:
         r = ruleSet[0]
         ruleSet.remove(r)
-        
+        #change intersected = [] by a variable
         intersected = [ ]
         intersected.append(r)
-        I = []
-        I.append(r)
+
+        connectedComponent = []
+        connectedComponent.append(r)
         
         while intersected:
             rule1 = intersected[0]
@@ -69,26 +84,36 @@ def createConnectedComponents(ruleSet):
                 print('comparing', rule1, rule2)
                 if intersection( rule1, rule2 ) and rule1!=rule2:
                     print('intersection')
-                    I.append(rule2)
-            print('I',I)
+                    connectedComponent.append(rule2)
+            print('connectedComponent',connectedComponent)
             intersected.remove(rule1)
         print('--------------------------------------')
-        print('connected component', I )
+        print('connected component', connectedComponent )
         subset = False
-        for component in connectedComponents:
-            if all(x in component for x in I):
+        for element in connectedComponents:
+            #si al menos uno esta en el componente anterior agrega las que no estan FALTA*********
+            for x in element:
+               if x in connectedComponent:
+                   [connectedComponent.append(x) for x in element if x not in connectedComponent]
+            #if its a subset don't do anything
+            if all(x in element for x in connectedComponent):
                 subset = True
         if subset == False:
-            connectedComponents.append(I)
+            connectedComponents.append(connectedComponent)
+    
+    #Delete lists that are contained in others
+    #finalConnectedComponents = []
+    for component1 in connectedComponents:
+        #is_subset=False
+        for component2 in connectedComponents:
+            if component1 != component2 and all(x in component2 for x in component1):
+                print('this list is a subset', component1)
+                connectedComponents.remove(component1)
+                #is_subset = True
+            #elif component1 not in finalConnectedComponents and is_subset == False:
+             #   finalConnectedComponents.append(component1)
+    print('final connected components : ')
     [print(i) for i in connectedComponents]
     return connectedComponents
-
-
-
-
-
-
-
-
-
+print('using the following rule set',ruleSet)
 createConnectedComponents(ruleSet)
